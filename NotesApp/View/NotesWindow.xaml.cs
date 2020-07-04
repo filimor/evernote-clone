6cd776a8c24b985af3d5b0ecb3987e8d80226f27
Filipe Moreira <filimor@posteo.com.br>
2020-07-04 17:51:24 -0300

    Implement the Speech Recognizer

--- NotesApp/View/NotesWindow.xaml.cs
+++ NotesApp/View/NotesWindow.xaml.cs
@@ -1,27 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Speech.Recognition;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NotesApp.View
 {
     /// <summary>
-    /// Interaction logic for NotesWindow.xaml
+    ///     Interaction logic for NotesWindow.xaml
     /// </summary>
     public partial class NotesWindow : Window
     {
+        private SpeechRecognitionEngine _recognizer;
+        private bool _isRecognizing = false;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            RecognizerInfo currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+                where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                select r).FirstOrDefault();
+            _recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            var builder = new GrammarBuilder();
+            builder.AppendDictation();
+            var grammar = new Grammar(builder);
+            _recognizer.LoadGrammar(grammar);
+            _recognizer.SetInputToDefaultAudioDevice();
+
+            _recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            ContentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(e.Result.Text)));
         }
 
         private void MenuItem_OnClick(object sender, RoutedEventArgs e)
@@ -29,14 +43,25 @@ namespace NotesApp.View
             Application.Current.Shutdown();
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private void SpeechButton_OnClick(object sender, RoutedEventArgs e)
         {
-            //TODO: Speech recognition
+            if (!_isRecognizing)
+            {
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                _isRecognizing = true;
+            }
+            else
+            {
+                _recognizer.RecognizeAsyncStop();
+                _isRecognizing = false;
+            }
         }
 
         private void ContentRichTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            int amountOfCharacters = new TextRange(ContentRichTextBox.Document.ContentStart, ContentRichTextBox.Document.ContentEnd).Text.Length;
+            int amountOfCharacters =
+                new TextRange(ContentRichTextBox.Document.ContentStart, ContentRichTextBox.Document.ContentEnd).Text
+                    .Length;
             StatusTextBlock.Text = $"Document length: {amountOfCharacters} characters";
         }
 
